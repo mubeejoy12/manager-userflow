e09cc4f2826c00660a422598afa208cc7ae7a1c5
mubarak <abubakrobasanjo@gmail.com>
2025-07-10 06:30:52 -0700

    recent update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import "./globals.css";
+// app/components/DashboardLayout.tsx
+import ClientLayout from "./ClientLayout";
+// import { useState } from "react";
 
 export const metadata = {
   title: "Sinkronis",
@@ -10,11 +13,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {/* <header className="p-4 flex items-center">
-          <Image src="/logo.png" alt="Logo" width={164} height={48} />
-         
-        </header> */}
-        <main>{children}</main>
+        <ClientLayout>
+          <div >
+       
+            {/* Add dashboard-specific styles */}
+            {children}
+          </div>
+        </ClientLayout>
       </body>
     </html>
   );
