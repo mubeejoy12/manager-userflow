d94a93b0f72aaf98dcdb4ce10c548963ec73236c
mubarak <abubakrobasanjo@gmail.com>
2025-07-19 00:49:17 -0700

    deaclear them with variable and subset

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,15 @@ import { Geist, Geist_Mono } from "next/font/google";
 import ClientLayout from "./ClientLayout";
 import { Outfit } from "next/font/google";
 // import { useState } from "react";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 export const metadata = {
   title: "Sinkronis",
   description: "Signup to get started",
