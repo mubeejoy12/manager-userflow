a0deb34f7a3bee212d64c05d92f05c05ab4f78ec
mubarak <abubakrobasanjo@gmail.com>
2025-07-19 00:37:32 -0700

    added a genneral font

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true, // Optional: Enables React strict mode
+    allowedDevOrigins: ["http://192.168.0.162", "http://localhost:3002", "http://192.168.0.162:3002"],
+  };
+  
+  export default nextConfig;
