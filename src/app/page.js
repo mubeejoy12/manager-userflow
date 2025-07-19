c16a0dd0a9717a5ce3e6f3eff389c9117d601324
mubarak <abubakrobasanjo@gmail.com>
2025-07-19 01:09:18 -0700

    added login route and added overview page after login

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,22 @@
-import Image from "next/image";
-import CreatePassword from "./signup/page";
-import DashboardLayout from "./components/DashboardLayout";
-import { redirect } from 'next/navigation';
-// import { CreatePassword } from "./signup/page";
+"use client"; 
+
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  redirect('/overview');
-  // return (
-  //   <div className="">
-  //     <DashboardLayout />
-  //     <CreatePassword />
-  //   </div>
-  // );
+  const router = useRouter();
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      router.push("/overview");
+    } else {
+      router.push("/login");
+    }
+  }, []);
+
+  return null;
 }
+
+
