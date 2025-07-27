c7e880a55e11dec4e505496dd2f65cac2585c325
mubarak <abubakrobasanjo@gmail.com>
2025-07-27 04:31:47 -0700

    modify code

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true, // Optional: Enables React strict mode
-    allowedDevOrigins: ["http://192.168.0.162", "http://localhost:3002", "http://192.168.0.162:3002"],
+    allowedDevOrigins: ["http://192.168.0.162", "http://localhost:3000", "http://192.168.0.162:3002"],
   };
   
   export default nextConfig;
