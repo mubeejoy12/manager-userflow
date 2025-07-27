ff12a4a6a7dc352c03f609537fe056038c48bad4
mubarak <abubakrobasanjo@gmail.com>
2025-07-27 04:54:15 -0700

    added protected route

---
+++ src/app/components/ProtectedRoute.js
@@ -0,0 +1,22 @@
+"use client";
+
+import { useAuth } from "../context/AuthContext";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function ProtectedRoute({ children }) {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push("/auth/login");
+    }
+  }, [loading, user]);
+
+  if (loading) return null; // or a spinner
+  if (!user) return null;
+
+  return children;
+}
+
